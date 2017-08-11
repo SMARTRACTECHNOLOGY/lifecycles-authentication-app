d18aa59f4a7b3afed61e9bdd1d19f83010a7f5e8
smitch88 <scottymitch88@gmail.com>
2017-08-11 10:39:45 -0400

    Add scanning code

--- src/screens/ScanScreen/index.js
+++ src/screens/ScanScreen/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AsyncStorage, Image, Text, View } from 'react-native';
+import { AsyncStorage, Image, Text, ToastAndroid, View } from 'react-native';
+import NFC, { NfcDataType, NdefRecordType } from 'react-native-nfc';
 import { Button, NavHeader, Screen } from '../../components';
 import styles from './styles';
 
@@ -7,6 +8,45 @@ export default class ScanScreen extends React.Component {
 
   navigateToScanDisplay = () => {
     this.props.navigation.navigate('Display');
+  }
+
+  handleNdef (payload) {
+    const { data, id } = payload;
+    for (const dataIndex in data) {
+      const records = data[dataIndex];
+      for (let recordIndex in records) {
+        const { type, data } = records[recordIndex];
+        console.log('read ndef tag', id, type, data);
+        if (type === NdefRecordType.TEXT) {
+          try {
+            NFC.removeListener('NFC_CHIP');
+            this.navigateToScan({ id, data });
+          } catch (e) {
+            this.navigateToScan({ id, data });
+          }
+        } else {
+          ToastAndroid.show(`Non-TEXT tag of type ${type} with data ${data}`, ToastAndroid.SHORT);
+        }
+      }
+    }
+  }
+
+  bindNfcListener () {
+    NFC.addListener('NFC_CHIP', (payload) => {
+      switch (payload.type) {
+        case NfcDataType.NDEF:
+          ToastAndroid.show(`NFC Tag Detected`, ToastAndroid.SHORT);
+          this.handleNdef(payload);
+          break;
+        case NfcDataType.TAG:
+          ToastAndroid.show('Invalid Tag Type Detected. Try Again.', ToastAndroid.SHORT);
+          break;
+      }
+    });
+  }
+
+  componentDidMount () {
+    this.bindNfcListener();
   }
 
   render(){
