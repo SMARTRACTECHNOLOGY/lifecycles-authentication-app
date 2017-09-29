fb6b0cba4adeeb9bd8ed8411b771dce6176ced3f
nicholasbrady <nick.brady@smartrac-group.com>
2017-09-29 15:10:16 -0700

    Use Auth0 for authentication

---
+++ src/screens/AuthScreen/auth0-credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientId: "58N1xpMIGYXaDGZdlb3ouN5Yj3aNilC8",
+  domain: "lifecycles.eu.auth0.com"
+};
--- src/screens/AuthScreen/index.js
+++ src/screens/AuthScreen/index.js
@@ -1,62 +1,65 @@
-import React from 'react';
-import { ActivityIndicator, AsyncStorage, TextInput, View } from 'react-native';
-import { Button, Login, Screen } from '../../components';
+import React, { Component } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  AppRegistry,
+  AsyncStorage,
+  Platform,
+  Text,
+  View
+} from 'react-native';
+import { Button, Screen } from '../../components';
+import Auth0 from 'react-native-auth0';
 import theme from '../../theme';
 import styles from './styles';
 
+var credentials = require('./auth0-credentials');
+const auth0 = new Auth0(credentials);
+
 export default class AuthScreen extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.getExisting()
     this.state = {
-      error: undefined,
-      username: '',
-      isAuthenticating: false
-    }
+      jwt: null,
+      isAuthenticating: true
+    };
   }
 
-  handleAuthenicationError = (err) => {
-    this.setState({
-      error: 'Invalid username or password',
-      isAuthenticating: false
-    });
-  }
-
-  handleAuthenicationSuccess = (username, jwt) => {
-    this.setState({
-      error: undefined,
-      isAuthenticating: false
-    });
-    // Store session information and proceed to `Scan` screen
-    const appState = JSON.stringify({ username, jwt });
-    AsyncStorage.setItem(this.props.storageKey, appState)
-      .then(() => {
-        // Navigate to the scan screen
+  getExisting = () => {
+    console.log("getting the existing app state...")
+    AsyncStorage.getItem(this.props.storageKey).then((value) => {
+      const { jwt } = JSON.parse(value)
+      this.setState({ jwt, isAuthenticating: false })
+      if (jwt) {
         this.props.navigation.navigate('Scan');
+      } else {
+        this.onLogin()
+      }
+    })
+  }
+
+  onLogin = () => {
+    auth0.webAuth
+      .authorize({
+        scope: 'openid profile',
+        audience: 'https://' + credentials.domain + '/userinfo'
+      })
+      .then(({ accessToken }) => {
+        this.setState({ jwt: accessToken, isAuthenticating: false });
+        AsyncStorage.mergeItem(this.props.storageKey, JSON.stringify({ jwt: accessToken }))
+          .then(() => {
+            this.props.navigation.navigate('Scan');
+          });
+      })
+      .catch(error => {
+        this.setState({ isAuthenticating: false })
+        console.log(error)
       });
-  }
-
-  signIn = (username, password) => {
-    this.setState({
-      isAuthenticating: true,
-      username
-    });
-    this.props.databroker
-      .authenticate(username, password)
-      .then(this.handleAuthenicationSuccess.bind(this, username))
-      .catch(this.handleAuthenicationError)
-  }
-
-  componentDidMount(){
-    if(this.props.navigation.state.params){
-      console.log('current username!', this.props.navigation.state.params.username);
-      this.setState({ username: this.props.navigation.state.params.username})
-    }
-  }
-
-  render(){
-    const { error, username, isAuthenticating } = this.state;
-    const loadingDisplay = isAuthenticating ? 'flex' : 'none';
+  };
+  
+  render() {
+    const loadingDisplay = this.state.isAuthenticating ? 'flex' : 'none';
     return (
       <Screen
         id="auth-screen"
@@ -64,19 +67,22 @@ export default class AuthScreen extends React.Component {
       >
         <View style={ [ styles.loading, { display: loadingDisplay }] }>
           <ActivityIndicator
-            animating={ isAuthenticating }
+            animating={ this.state.isAuthenticating }
             color={ theme.color.lightBackground }
             size={ theme.loading.size }
           />
         </View>
-        <View style={ styles.login }>
-          <Login
-            error={ error }
-            username={ username }
-            onSubmit={ this.signIn }
+        <View style={ styles.container }>
+        <Text style={styles.header}> You will be redirected for authentication.</Text>
+          <Text style={styles.subtext}> Please click the login button if the page doesn't load automatically</Text>
+          <Button
+            onPress={ this.onLogin }
+            style={ styles.button }
+            title="Log In"
           />
         </View>
       </Screen>
     );
   }
 }
+
